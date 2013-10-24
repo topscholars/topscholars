0b561de287642afffda223b4dd24d86ef3361eb0
rittidate <rittidate.k@trueclarityintl.com>
2013-10-24 18:10:35 +0700

    add student in classlist
    add student in classlist

--- ajaxs.py
+++ ajaxs.py
@@ -6,8 +6,10 @@ from django.db.models.query import RawQuerySet
 from django.db import connection
 from tsweb.models import *
 from django.db.models import Q
+from itertools import chain
 
 from datetime import datetime
+import time
 
 class CLASSLIST():
     def get(self,request):
@@ -63,7 +65,7 @@ class CLASSLIST():
             clientid = login.clientid
             if login.usertypeid==1:
                 recid = Login.objects.filter(usertypeid=1).values_list('recid')
-                user = list(Userlist.objects.filter(id__in=recid,disabled=0,deleted=0,clientid=clientid).values('id','firstname','middlename','lastname'))
+                user = list(Userlist.objects.filter(id__in=recid,clientid=clientid).values('id','firstname','middlename','lastname'))
                 data = simplejson.dumps(user)
         return HttpResponse(data, mimetype='application/json')
     
@@ -144,7 +146,100 @@ class CLASSLIST():
             classschedule.clientid = clientid
             classschedule.save()
         return HttpResponse('success', mimetype='application/json')
+    
+    def getTStudentList(self,request):
+        try:
+            userid = request.session['userid']
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            classid = request.GET.get('classid', False)
+            studentid = Studentclass.objects.filter(classscheduleid=classid,disabled=0,deleted=0,clientid=clientid).values_list('studentid')
+            studentlist = Studentlist.objects.filter(id__in=studentid)
+            context = {'studentlist': studentlist}
+            return render(request, 'tsweb/teacher/classlist_studentlistajax.html', context)
+    
+    def getTStudentAddList(self,request):
+        try:
+            cursor = connection.cursor()
+            userid = request.session['userid']
+            classid = request.GET.get('classid', False)
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            classschedule = Classschedule.objects.get(clientid=clientid, id=classid)
+            studentlist = Studentclass.objects.filter(clientid=clientid).values_list('studentid')
+            studentObj1 = Studentlist.objects.filter(~Q(id__in = studentlist)).values_list('id')
+            #cursor.execute("select sl.id,cs.startdate,cs.enddate,cs.starttime,cs.endtime,cs.dayofweek from studentlist as sl join studentclass as sc on (sl.id = sc.studentid) join classschedule as cs on (sc.classscheduleid = cs.id) where sc.clientid=%s and (sc.classscheduleid = %s and sc.status = 1 or !(sc.classscheduleid = %s))",[clientid, classid, classid])
+            cursor.execute("select sl.id,cs.startdate,cs.enddate,cs.starttime,cs.endtime,cs.dayofweek from studentlist as sl join studentclass as sc on (sl.id = sc.studentid) join classschedule as cs on (sc.classscheduleid = cs.id) where sc.clientid=%s  and ((sc.classscheduleid = %s and sc.status = 1)  or sc.studentid not in (select sc2.studentid from studentclass sc2 where sc2.classscheduleid = %s ))",[clientid, classid, classid])
+            classStartDate = int(time.mktime(classschedule.startdate.timetuple()))
+            classEndDate = int(time.mktime(classschedule.enddate.timetuple()))
+            classStartTime = int(classschedule.starttime.replace(':',''))
+            classEndTime = int(classschedule.endtime.replace(':',''))
+            classDayofweek = classschedule.dayofweek.split(',')
 
+            resultid = []
+            for obj in studentObj1:
+                resultid.append(obj[0])
+                
+            for row in cursor.fetchall():
+                studentId = row[0]
+                startdate = int(time.mktime(row[1].timetuple()))
+                enddate = int(time.mktime(row[2].timetuple()))
+                starttime = int(row[3].replace(':',''))
+                endtime = int(row[4].replace(':',''))
+                dayofweek = row[5].split(',')
+                if (classStartDate > startdate and classStartDate > enddate) or (classEndDate < startdate and classEndDate < enddate):
+                    resultid.append(studentId)
+                else:
+                    haveday = 0
+                    for day in dayofweek:
+                        if day in classDayofweek:
+                            haveday = 1
+                            break
+                    if haveday == 1:
+                        if (classStartTime > starttime and classStartTime > endtime) or (classEndTime < starttime and classEndTime < endtime):
+                            resultid.append(studentId)
+                    else:
+                        resultid.append(studentId)
+                        
+            studentListResult = Studentlist.objects.filter(id__in = resultid)
+            context = {'studentlist': studentListResult}
+            return render(request, 'tsweb/teacher/classlist_studentaddajax.html', context)
+        
+    def addStudentInClass(self,request):
+        try:
+            userid = request.session['userid']
+            classid = request.GET.get('classid', False)
+            studentid = request.GET.get('studentid', False)
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            
+            studentlist = Studentlist.objects.get(id=studentid)
+            classschedulelist = Classschedule.objects.get(id=classid)
+            
+            studentclass = Studentclass()
+            studentclass.studentid = studentlist
+            studentclass.classscheduleid = classschedulelist
+            studentclass.grade = 0
+            studentclass.status = 0
+            studentclass.createddt = datetime.now()
+            studentclass.createdby = userid
+            studentclass.modifieddt = datetime.now()
+            studentclass.modifiedby = userid
+            studentclass.disabled = 0
+            studentclass.deleted = 0
+            studentclass.clientid = clientid
+            studentclass.save()
+            return HttpResponse('success')
+            
 class ASSSIGNMENTLIST():
     def get(self,request):
         cursor = connection.cursor()
@@ -250,6 +345,11 @@ class STUDENTLIST():
                    
             results = cursor.fetchall() 
             for r in results:
+                if r[16] is not None:
+                    dob = r[16].strftime(DATE_FORMAT)
+                else: 
+                    dob = ''
+                    
                 data_json = {
                         'id': r[0],
                         'firstname': r[1],
@@ -267,7 +367,7 @@ class STUDENTLIST():
                         'otherphone': r[13],
                         'emailaddress1': r[14],
                         'emailaddress2': r[15],
-                        'dob': r[16].strftime(DATE_FORMAT),
+                        'dob': dob,
                         'gender': r[17],
                         'salutation': r[18],
                         'currentaccademicyear': r[19],

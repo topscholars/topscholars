76864f95e5d5aa09d092c7a4d390c44c636e6bc4
eakpolattrueclarity <eakpol.s@trueclarityintl.com>
2014-01-16 14:49:57 +0700

    Fix Load New Master

---
+++ ajax/classajax.py
@@ -0,0 +1,268 @@
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.utils import simplejson
+from django.core import serializers
+from django.db.models.query import RawQuerySet
+from django.db import connection
+from tsweb.models import *
+from django.db.models import Q
+from itertools import chain
+
+from datetime import datetime
+import time
+
+class CLASSLIST():
+    def get(self,request):
+        cursor = connection.cursor()
+        DATE_FORMAT = "%d-%m-%Y" 
+        try:
+            id = request.GET.get('id', False)
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            #classlist = Classschedule.objects.filter(id=id)
+            cursor.execute("SELECT id,code,subcode,classid,teacherid,startdate,enddate,starttime,endtime,disabled,dayofweek FROM classschedule  WHERE id = %s", [id])
+            
+            results = cursor.fetchall()
+            for r in results:
+                data_json = {
+                        'id': r[0],
+                        'code': r[1],
+                        'subcode': r[2],
+                        'classid': r[3],
+                        'teacherid': r[4],
+                        'startdate': r[5].strftime(DATE_FORMAT),
+                        'enddate': r[6].strftime(DATE_FORMAT),
+                        'starttime': r[7],
+                        'endtime': r[8],
+                        'disabled': r[9],
+                        'dayofweek': r[10],
+                        }
+            data = simplejson.dumps(data_json)
+        return HttpResponse(data, mimetype='application/json')
+    
+    def getClass(self,request):
+        try:
+            userid = request.session['userid']
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            if login.usertypeid==1:
+                recid = Login.objects.filter(usertypeid=1).values_list('recid')
+                classlist = list(Classlist.objects.filter(rubricid__in=recid,disabled=0,deleted=0,clientid=clientid).values('id','classname'))
+                data = simplejson.dumps(classlist)
+        return HttpResponse(data, mimetype='application/json')
+    
+    def getTeacher(self,request):
+        try:
+            userid = request.session['userid']
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            if login.usertypeid==1:
+                recid = Login.objects.filter(usertypeid=1).values_list('recid')
+                user = list(Userlist.objects.filter(id__in=recid,clientid=clientid).values('id','firstname','middlename','lastname'))
+                data = simplejson.dumps(user)
+        return HttpResponse(data, mimetype='application/json')
+    
+    def save(self,request):
+        DATE_FORMAT = "%d-%m-%Y" 
+        try:
+            userid = request.session['userid']
+            id = request.POST.get('id', False)
+            classid= request.POST.get('classid', False)
+            code = request.POST.get('code', False)
+            subcode = request.POST.get('subcode', False)
+            dayofweek = request.POST.get('dayofweek', False)
+            disabled = request.POST.get('disabled', False)
+            enddate = request.POST.get('enddate', False)
+            startdate = request.POST.get('startdate', False)
+            endtime = request.POST.get('endtime', False)
+            starttime = request.POST.get('starttime', False)
+            teacherid = request.POST.get('teacherid', False)
+        except KeyError:
+            data_json = { 'status': 'error', }
+            data = simplejson.dumps(data_json)
+            return HttpResponse(data, mimetype='application/json')
+        else:
+            classlist = Classlist.objects.get(id=classid)
+            classschedule = Classschedule.objects.get(id=id)
+            classschedule.classid = classlist
+            classschedule.code = code
+            classschedule.subcode = subcode
+            classschedule.disabled = disabled
+            classschedule.endtime = endtime
+            classschedule.starttime = starttime
+            classschedule.teacherid = teacherid
+            classschedule.dayofweek = dayofweek
+            classschedule.enddate = datetime.strptime(enddate,DATE_FORMAT)
+            classschedule.startdate = datetime.strptime(startdate,DATE_FORMAT)
+            classschedule.modifieddt = datetime.now()
+            classschedule.modifiedby = userid
+            classschedule.save()
+            data_json = { 'status': 'success', }
+            data = simplejson.dumps(data_json)
+            return HttpResponse(data, mimetype='application/json')
+    
+    def add(self,request):
+        DATE_FORMAT = "%d-%m-%Y" 
+        try:
+            userid = request.session['userid']
+            id = request.POST.get('id', False)
+            classid= request.POST.get('classid', False)
+            code = request.POST.get('code', False)
+            subcode = request.POST.get('subcode', False)
+            dayofweek = request.POST.get('dayofweek', False)
+            disabled = request.POST.get('disabled', False)
+            enddate = request.POST.get('enddate', False)
+            startdate = request.POST.get('startdate', False)
+            endtime = request.POST.get('endtime', False)
+            starttime = request.POST.get('starttime', False)
+            teacherid = request.POST.get('teacherid', False)
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            
+            classlist = Classlist.objects.get(id=classid)
+            
+            classschedule = Classschedule()
+            classschedule.classid = classlist
+            classschedule.code = code
+            classschedule.subcode = subcode
+            classschedule.disabled = disabled
+            classschedule.endtime = endtime
+            classschedule.starttime = starttime
+            classschedule.teacherid = teacherid
+            classschedule.dayofweek = dayofweek
+            classschedule.enddate = datetime.strptime(enddate,DATE_FORMAT)
+            classschedule.startdate = datetime.strptime(startdate,DATE_FORMAT)
+            classschedule.createddt = datetime.now()
+            classschedule.createdby = userid
+            classschedule.modifieddt = datetime.now()
+            classschedule.modifiedby = userid
+            classschedule.deleted = 0
+            classschedule.clientid = clientid
+            classschedule.save()
+            data_json = { 'status': 'success', }
+            data = simplejson.dumps(data_json)
+            return HttpResponse(data, mimetype='application/json')
+    
+    def getTStudentList(self,request):
+        try:
+            userid = request.session['userid']
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            classid = request.GET.get('classid', False)
+            studentid = Studentclass.objects.filter(classscheduleid=classid,disabled=0,deleted=0,clientid=clientid).values_list('studentid')
+            studentlist = Studentlist.objects.filter(id__in=studentid)
+            context = {'studentlist': studentlist}
+            return render(request, 'tsweb/teacher/classlist_studentlistajax.html', context)
+    
+    def getTStudentAddList(self,request):
+        try:
+            cursor = connection.cursor()
+            userid = request.session['userid']
+            classid = request.GET.get('classid', False)
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            classschedule = Classschedule.objects.get(clientid=clientid, id=classid)
+            studentlist = Studentclass.objects.filter(clientid=clientid).values_list('studentid')
+            studentObj1 = Studentlist.objects.filter(~Q(id__in = studentlist)).values_list('id')
+            #cursor.execute("select sl.id,cs.startdate,cs.enddate,cs.starttime,cs.endtime,cs.dayofweek from studentlist as sl join studentclass as sc on (sl.id = sc.studentid) join classschedule as cs on (sc.classscheduleid = cs.id) where sc.clientid=%s and (sc.classscheduleid = %s and sc.status = 1 or !(sc.classscheduleid = %s))",[clientid, classid, classid])
+            cursor.execute("select sl.id,cs.startdate,cs.enddate,cs.starttime,cs.endtime,cs.dayofweek from studentlist as sl join studentclass as sc on (sl.id = sc.studentid) join classschedule as cs on (sc.classscheduleid = cs.id) where sc.clientid=%s  and ((sc.classscheduleid = %s and sc.status = 1)  or sc.studentid not in (select sc2.studentid from studentclass sc2 where sc2.classscheduleid = %s ))",[clientid, classid, classid])
+            classStartDate = int(time.mktime(classschedule.startdate.timetuple()))
+            classEndDate = int(time.mktime(classschedule.enddate.timetuple()))
+            classStartTime = int(classschedule.starttime.replace(':',''))
+            classEndTime = int(classschedule.endtime.replace(':',''))
+            classDayofweek = classschedule.dayofweek.split(',')
+
+            resultid = []
+            for obj in studentObj1:
+                resultid.append(obj[0])
+                
+            for row in cursor.fetchall():
+                studentId = row[0]
+                startdate = int(time.mktime(row[1].timetuple()))
+                enddate = int(time.mktime(row[2].timetuple()))
+                starttime = int(row[3].replace(':',''))
+                endtime = int(row[4].replace(':',''))
+                dayofweek = row[5].split(',')
+                if (classStartDate > startdate and classStartDate > enddate) or (classEndDate < startdate and classEndDate < enddate):
+                    resultid.append(studentId)
+                else:
+                    haveday = 0
+                    for day in dayofweek:
+                        if day in classDayofweek:
+                            haveday = 1
+                            break
+                    if haveday == 1:
+                        if (classStartTime > starttime and classStartTime > endtime) or (classEndTime < starttime and classEndTime < endtime):
+                            resultid.append(studentId)
+                    else:
+                        resultid.append(studentId)
+                        
+            studentListResult = Studentlist.objects.filter(id__in = resultid)
+            context = {'studentlist': studentListResult}
+            return render(request, 'tsweb/teacher/classlist_studentaddajax.html', context)
+        
+    def addStudentInClass(self,request):
+        try:
+            userid = request.session['userid']
+            classid = request.POST.get('classid', False)
+            studentid = request.POST.get('studentid', False)
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            
+            studentlist = Studentlist.objects.get(id=studentid)
+            classschedulelist = Classschedule.objects.get(id=classid)
+            
+            studentclass = Studentclass()
+            studentclass.studentid = studentlist
+            studentclass.classscheduleid = classschedulelist
+            studentclass.grade = 0
+            studentclass.status = 0
+            studentclass.createddt = datetime.now()
+            studentclass.createdby = userid
+            studentclass.modifieddt = datetime.now()
+            studentclass.modifiedby = userid
+            studentclass.disabled = 0
+            studentclass.deleted = 0
+            studentclass.clientid = clientid
+            studentclass.save()
+            data_json = { 'status': 'success', }
+            data = simplejson.dumps(data_json)
+            return HttpResponse(data, mimetype='application/json')
+        
+class TCLASSLISTAJAX():
+    def get(self,request):
+        try:
+            userid = request.session['userid']
+        except KeyError:
+            return HttpResponseRedirect(reverse('tsweb:login'))
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            description=request.GET.get('description',False)
+            classlist = ''
+            if description == False or description == '':
+                classlist = Classschedule.objects.filter(clientid=clientid,deleted=0)
+            else:
+                classlist = Classschedule.objects.filter(clientid=clientid,classid__description__contains=description,deleted=0)
+            context = {'classlist': classlist}
+            return render(request, 'tsweb/teacher/classlistajax.html', context)
+            
---
+++ ajax/studentajax.py
@@ -0,0 +1,244 @@
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.utils import simplejson
+from django.core import serializers
+from django.db.models.query import RawQuerySet
+from django.db import connection
+from tsweb.models import *
+from django.db.models import Q
+from itertools import chain
+
+from datetime import datetime
+import time
+
+class STUDENTLIST():
+    def get(self,request):
+        cursor = connection.cursor()
+        DATE_FORMAT = "%d-%m-%Y" 
+        try:
+            id = request.GET.get('id', False)
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:
+            #classlist = Classschedule.objects.filter(id=id)
+            cursor.execute("SELECT id,firstname,lastname,middlename,address1,address2,address3,city,zipcode,state,country,mobilephone,homephone,otherphone,emailaddress1,emailaddress2,dob,gender,salutation,currentaccademicyear FROM studentlist  WHERE id = %s", [id])
+                   
+            results = cursor.fetchall() 
+            for r in results:
+                if r[16] is not None:
+                    dob = r[16].strftime(DATE_FORMAT)
+                else: 
+                    dob = ''
+                    
+                data_json = {
+                        'id': r[0],
+                        'firstname': r[1],
+                        'lastname': r[2],
+                        'middlename': r[3],
+                        'address1': r[4],
+                        'address2': r[5],
+                        'address3': r[6],
+                        'city': r[7],
+                        'zipcode': r[8],
+                        'state': r[9],
+                        'country': r[10],
+                        'mobilephone': r[11],
+                        'homephone': r[12],
+                        'otherphone': r[13],
+                        'emailaddress1': r[14],
+                        'emailaddress2': r[15],
+                        'dob': dob,
+                        'gender': r[17],
+                        'salutation': r[18],
+                        'currentaccademicyear': r[19],
+                        }
+            data = simplejson.dumps(data_json)
+        return HttpResponse(data, mimetype='application/json')
+    
+    def getGender(self,request):
+        try:
+            userid = request.session['userid']
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:  
+            select = 'Gender'
+            obj = Selectiongroup.objects.get(groupname=select)
+            id = obj.id
+            selectlist = list(Selectionlist.objects.filter(selectiongroupid=id,disabled=0,deleted=0).values('id','selectionname'))
+            data = simplejson.dumps(selectlist)
+        return HttpResponse(data, mimetype='application/json')
+    
+    def getSalutation(self,request):
+        try:
+            userid = request.session['userid']
+        except KeyError:
+            return HttpResponse('error', mimetype='application/json')
+        else:  
+            select = 'Salutation'
+            obj = Selectiongroup.objects.get(groupname=select)
+            id = obj.id
+            selectlist = list(Selectionlist.objects.filter(selectiongroupid=id,disabled=0,deleted=0).values('id','selectionname'))
+            data = simplejson.dumps(selectlist)
+        return HttpResponse(data, mimetype='application/json')
+    
+    def save(self,request):
+        DATE_FORMAT = "%d-%m-%Y"
+        try:
+            userid = request.session['userid']
+            id = request.POST.get('id', False)
+            firstname = request.POST.get('firstname', False)
+            lastname = request.POST.get('lastname', False)
+            middlename = request.POST.get('middlename', False)
+            address1 = request.POST.get('address1', False)
+            address2 = request.POST.get('address2', False)
+            address3 = request.POST.get('address3', False)
+            city = request.POST.get('city', False)
+            zipcode = request.POST.get('zipcode', False)
+            state = request.POST.get('state', False)
+            country = request.POST.get('country', False)
+            mobilephone = request.POST.get('mobilephone', False)
+            homephone = request.POST.get('homephone', False)
+            otherphone = request.POST.get('otherphone', False)
+            emailaddress1 = request.POST.get('emailaddress1', False)
+            emailaddress2 = request.POST.get('emailaddress2', False)
+            dob = request.POST.get('dob', False)
+            gender = request.POST.get('gender', False)
+            salutation = request.POST.get('salutation', False)
+            currentaccademicyear = request.POST.get('currentaccademicyear', False)
+        except KeyError:
+            data_json = { 'status': 'error', }
+            data = simplejson.dumps(data_json)
+            return HttpResponse(data, mimetype='application/json')
+        else:
+            studentlist = Studentlist.objects.get(id=id)
+            studentlist.firstname = firstname
+            studentlist.lastname = lastname
+            studentlist.middlename = middlename
+            studentlist.address1 = address1
+            studentlist.address2 = address2
+            studentlist.address3 = address3
+            studentlist.city = city
+            studentlist.zipcode = zipcode
+            studentlist.state = state
+            studentlist.country = country
+            studentlist.mobilephone = mobilephone
+            studentlist.homephone = homephone
+            studentlist.otherphone = otherphone
+            studentlist.emailaddress1 = emailaddress1
+            studentlist.emailaddress2 = emailaddress2
+            studentlist.dob = datetime.strptime(dob,DATE_FORMAT)
+            studentlist.gender = gender
+            studentlist.salutation = salutation
+            studentlist.currentaccademicyear = currentaccademicyear
+            studentlist.save()
+            data_json = { 'status': 'success', }
+            data = simplejson.dumps(data_json)
+            return HttpResponse(data, mimetype='application/json')
+        
+    def add(self,request):
+        DATE_FORMAT = "%d-%m-%Y"
+        try:
+            userid = request.session['userid']
+            
+            user = request.POST.get('user', False)
+            password = request.POST.get('password', False)
+            hint = request.POST.get('hint', False)
+            
+            id = request.POST.get('id', False)
+            firstname = request.POST.get('firstname', False)
+            lastname = request.POST.get('lastname', False)
+            middlename = request.POST.get('middlename', False)
+            address1 = request.POST.get('address1', False)
+            address2 = request.POST.get('address2', False)
+            address3 = request.POST.get('address3', False)
+            city = request.POST.get('city', False)
+            zipcode = request.POST.get('zipcode', False)
+            state = request.POST.get('state', False)
+            country = request.POST.get('country', False)
+            mobilephone = request.POST.get('mobilephone', False)
+            homephone = request.POST.get('homephone', False)
+            otherphone = request.POST.get('otherphone', False)
+            emailaddress1 = request.POST.get('emailaddress1', False)
+            emailaddress2 = request.POST.get('emailaddress2', False)
+            dob = request.POST.get('dob', False)
+            gender = request.POST.get('gender', False)
+            salutation = request.POST.get('salutation', False)
+            currentaccademicyear = request.POST.get('currentaccademicyear', False)
+        except KeyError:
+            data_json = { 'status': 'error', }
+            data = simplejson.dumps(data_json)
+            return HttpResponse(data, mimetype='application/json')
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            
+            recid = Studentlist.objects.latest('id').id
+            
+            login = Login()
+            login.loginname = user
+            login.password = password
+            login.hint = hint
+            login.usertypeid = 2
+            login.recid = recid+1
+            login.modifieddt = datetime.now()
+            login.modifiedby = userid
+            login.createddt = datetime.now()
+            login.createdby = userid
+            login.disabled = 0
+            login.deleted = 0
+            login.clientid = clientid
+            login.save()
+            
+            studentlist = Studentlist()
+            studentlist.firstname = firstname
+            studentlist.lastname = lastname
+            studentlist.middlename = middlename
+            studentlist.address1 = address1
+            studentlist.address2 = address2
+            studentlist.address3 = address3
+            studentlist.city = city
+            studentlist.zipcode = zipcode
+            studentlist.state = state
+            studentlist.country = country
+            studentlist.mobilephone = mobilephone
+            studentlist.homephone = homephone
+            studentlist.otherphone = otherphone
+            studentlist.emailaddress1 = emailaddress1
+            studentlist.emailaddress2 = emailaddress2
+            studentlist.dob = datetime.strptime(dob,DATE_FORMAT)
+            studentlist.gender = gender
+            studentlist.salutation = salutation
+            studentlist.currentaccademicyear = currentaccademicyear
+            studentlist.timezone = 0
+            studentlist.otherphonetype = 0
+            studentlist.enrollmentdt = datetime.now()
+            studentlist.leadid = 0
+            studentlist.clientid = clientid
+            studentlist.save()
+            data_json = { 'status': 'success', }
+            data = simplejson.dumps(data_json)
+            return HttpResponse(data, mimetype='application/json')
+    
+class TSTUDENTLISTAJAX():
+    def get(self,request):
+        try:
+            userid = request.session['userid']
+        except KeyError:
+            return HttpResponseRedirect(reverse('tsweb:login'))
+        else:
+            login = Login.objects.get(id=userid)
+            clientid = login.clientid
+            studentname=request.GET.get('studentname',False)
+            classid=request.GET.get('classid',False)
+            studentlist = ''
+            activeid = Login.objects.filter(deleted=0,usertypeid=2).values_list('recid',flat=True)
+            if (studentname != False and studentname != '') and (classid != False and classid != ''):
+                studentlist = Studentlist.objects.filter(Q(clientid=clientid) & Q(id__in=activeid) & Q(Studentclasstostudents__classscheduleid__id=classid) & (Q(firstname__contains=studentname) | Q(middlename__contains=studentname) | Q(lastname__contains=studentname)))
+            elif studentname != False and studentname != '':
+                studentlist = Studentlist.objects.filter(Q(clientid=clientid) & Q(id__in=activeid) & (Q(firstname__contains=studentname) | Q(middlename__contains=studentname) | Q(lastname__contains=studentname)))
+            elif classid != False and classid != '':
+                studentlist = Studentlist.objects.filter(clientid=clientid,id__in=activeid,Studentclasstostudents__classscheduleid__id=classid)
+            else:
+                studentlist = Studentlist.objects.filter(clientid=clientid,id__in=activeid)
+            context = {'studentlist': studentlist}
+            return render(request, 'tsweb/teacher/studentlistajax.html', context)
